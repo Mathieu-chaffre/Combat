d7a7dd9b5e78dd733117bbe3d22aa7620c8e731e
Mathieu-chaffre <chaffrem@gmail.com>
2020-01-13 11:34:38 +0100

    mise en place de phrase d'attaque et des dégats a l'ennemoie

--- combat.c
+++ combat.c
@@ -1,4 +1,14 @@
 #include <stdio.h>
 int main(){
-
+int vie_joueur = 100;
+int vie_ennemi = 100;
+int attaque
+int epee = 10
+  printf("\ntu possede : %d hp", vie_joueur);
+  printf("\n L'ennemie a lui : %d hp", vie_ennemi);
+  printf("tu veux attaquer l'ennemie ? (1 oui et 2 non)");
+  printf("\ntu as attaquer l'ennemie avec ton epee", );
+  vie_ennemi = vie_ennemi-epee
+  printf("\nIl reste %d hp au monstre", vie_ennemi );
+  }
 }
