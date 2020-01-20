a5ce5d089b9df5184a91f27860d79890aa440c59
Mathieu-chaffre <chaffrem@gmail.com>
2020-01-20 13:20:31 +0100

    Update fonction.c

--- fonction.c
+++ fonction.c
@@ -31,6 +31,7 @@ void defense(){
  }
  if (decision_joueur == 4) {
    printf("Tu utilise une potion"\n");
+   poison_actif_ennemie = 0;
  }
     printf(" il reste % hp au monstre\n", vie_ennemi );
     printf("Il te reste %d hp\n", vie_joueur);
@@ -46,20 +47,67 @@ void attaque() {
     vie_joueur = vie_joueur - attaque_ennemi;
   }
   if (decision_joueur == 2) {
-    /* code */
+    printf("Defense\n");
+    int save_ennemie= attaque_ennemi;
+    attaque_ennemi = attaque_ennemi /2;
+    vie_joueur = vie_joueur - attaque_ennemi;
+    attaque_ennemi = save_ennemie;
   }
+  if (decision_joueur == 3) {
+    printf("Tu l'empoisonne\n");
+    poison_atcif = 1;
+    vie_joueur = vie_joueur - attaque_ennemi;
+  }
+  if (decision_joueur == 4) {
+    printf("potion\n");
+    poison_actif_ennemie = 0;
+    vie_joueur = vie_joueur - attaque_ennemi;
+  }
+  printf("il te reste %d hp\n", vie_joueur);
+  printf("Il reste %d hp au monstre\n", vie_ennemi );
+
+}
+
+void poison () {
+  if (decision_joueur == 1) {
+    printf("Attaque\n");
+    vie_ennemi = vie_ennemi -attaque_j
+    poison_actif_ennemie = 1;
+    printf("Il t'empoisonne\n");
+  }
+  if (decision_joueur == 2) {
+    printf("Il t'empoisonne\n");
+    poison_actif_ennemie = 1;
+  }
+  if (decision_joueur == 3) {
+    printf("Vous utiliser un sort d'empoisonnement tout les deux !\n");
+    poison_actif = 1;
+    poison_actif_ennemie = 1;
+  }
+  if (decision_joueur == 4) {
+    printf("Tu utilise une potion mais l'ennemie utilise un sort d'empoisonnennement\n");
+    poison_actif_ennemie = 1;
+  }
+
+  printf("il te reste %d hp\n", vie_joueur);
+  printf("Il lui reste %d hp\n", vie_ennemi );
 }
 
 
 main(){
   printf("choisi ton attaque :\n");
   scanf("%d", decision_joueur);
-  if (decision_joueur == 1) {
     srand(time(NULL));
     int nbgen=rand()%3+1;
     decision_ennemi = nbgen;
     if (decision_ennemi == 1) {
       defense()
+    }
+    if (decision_ennemi == 2) {
+      attaque()
+    }
+    if (decision_ennemi == 3) {
+      poison()
     }
   }
 
